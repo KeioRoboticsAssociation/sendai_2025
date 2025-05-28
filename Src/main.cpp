534bb9003be4609e7835dc8656dbe64430f27b85
imanoob <ymrs1122@gmail.com>
2025-05-28 17:44:59 +0900

    addmotor

--- Src/main.cpp
+++ Src/main.cpp
@@ -15,6 +15,10 @@ extern TIM_HandleTypeDef htim5; // 割り込み(20Hz)
 extern TIM_HandleTypeDef htim6; // 割り込み(100Hz)
 extern TIM_HandleTypeDef htim7; // 割り込み(1000Hz)
 
+DCMotor motor1(&htim1, TIM_CHANNEL_1, GPIOB, GPIO_PIN_1, true, 0.5f);
+DCMotor motor2(&htim1, TIM_CHANNEL_2, GPIOB, GPIO_PIN_4, true, 0.5f);
+DCMotor motor3(&htim1, TIM_CHANNEL_3, GPIOB, GPIO_PIN_15, true, 0.5f);
+
 void setup()
 {
     // ここに初期化処理を書く
